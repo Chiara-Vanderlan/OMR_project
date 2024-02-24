b8953561e5c4d597bcdb12ea9aa19752ed765edc
Chiara-Vanderlan <115263438+Chiara-Vanderlan@users.noreply.github.com>
2024-02-25 00:04:07 +0530

    stacked images

--- OMR_Main.py
+++ OMR_Main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import utlis
 
 ##########################
 path = "1.jpg"
@@ -8,12 +9,13 @@ heightImg = 700
 #########################
 img = cv2.imread(path)
 
-img = cv2.resize(img,(widthImg,heightImg))
-#imgGray = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-#imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
-#imgCanny =cv2.Canny(imgBlur,10,50)
+img = cv2.resize(img, (widthImg, heightImg))
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(img, (5, 5), 1)
+imgCanny = cv2.Canny(imgBlur, 10, 50)
 
+imageArray = ([img, imgGray,imgBlur,imgCanny])
+imgStacked = utlis.stackImages(imageArray, 0.5)
 
-
-cv2.imshow("Original", img)
+cv2.imshow("Stacked Images", imgStacked)
 cv2.waitKey(0)
